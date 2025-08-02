e69dd2bc0a69372ffdc969b207ad6c71ead3168f
Abdoaref739 <105456212+Abdoaref739@users.noreply.github.com>
2025-08-02 18:01:48 +0300

    Add files via upload

--- file.js
+++ file.js
@@ -1,10 +1,8 @@
-let reset_button = document.getElementById('reset-button');
 let happy_button = document.getElementById('happy-button');
 let sad_button = document.getElementById('sad-button');
 let angry_button = document.getElementById('angry-button');
 let navbar = document.getElementById('navbar');
 let container = document.getElementById('container');
-let mood = document.getElementById('mood-display');
 
 function resetMoodColors() {
     container.classList.remove('bg-red-500', 'bg-yellow-300', 'bg-green-500', 'bg-indigo-500');
@@ -15,31 +13,16 @@ happy_button.addEventListener('click', () => {
     resetMoodColors();
     container.classList.add('bg-yellow-300');
     navbar.classList.add('bg-yellow-700');
-    mood.innerHTML = 'your mood is Happy!';
 });
 
 sad_button.addEventListener('click', () => {
     resetMoodColors();
     container.classList.add('bg-green-500');
     navbar.classList.add('bg-green-700');
-    mood.innerHTML = 'your mood is Sad!';
 });
 
 angry_button.addEventListener('click', () => {
     resetMoodColors();
     container.classList.add('bg-red-500');
     navbar.classList.add('bg-red-700');
-    mood.innerHTML = 'your mood is Angryyy!';
 });
-reset_button.addEventListener('click', () => {
-    resetMoodColors();
-    container.classList.add('bg-indigo-500');
-    navbar.classList.add('bg-indigo-700');
-    mood.innerHTML = 'your mood is Neutral!';
-});
-
-window.onload = () => {
-    resetMoodColors();
-    container.classList.add('bg-indigo-500');
-    navbar.classList.add('bg-indigo-700');
-};
