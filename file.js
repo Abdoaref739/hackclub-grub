8431200adbaaea39b8a020ada9236ee5e69638b8
Abdoaref739 <105456212+Abdoaref739@users.noreply.github.com>
2025-08-02 18:06:00 +0300

    Add files via upload

--- file.js
+++ file.js
@@ -3,26 +3,15 @@ let sad_button = document.getElementById('sad-button');
 let angry_button = document.getElementById('angry-button');
 let navbar = document.getElementById('navbar');
 let container = document.getElementById('container');
-
-function resetMoodColors() {
-    container.classList.remove('bg-red-500', 'bg-yellow-300', 'bg-green-500', 'bg-indigo-500');
-    navbar.classList.remove('bg-red-700', 'bg-yellow-700', 'bg-green-700', 'bg-indigo-700');
-}
-
+let mood_display = document.getElementById('mood-display');
 happy_button.addEventListener('click', () => {
-    resetMoodColors();
-    container.classList.add('bg-yellow-300');
-    navbar.classList.add('bg-yellow-700');
+    mood_display.textContent = "Your current mood is: Happy 😊";
 });
 
 sad_button.addEventListener('click', () => {
-    resetMoodColors();
-    container.classList.add('bg-green-500');
-    navbar.classList.add('bg-green-700');
+    mood_display.textContent = "Your current mood is: sad 😢";
 });
 
 angry_button.addEventListener('click', () => {
-    resetMoodColors();
-    container.classList.add('bg-red-500');
-    navbar.classList.add('bg-red-700');
+    mood_display.textContent = "Your current mood is: angry 😠";
 });
